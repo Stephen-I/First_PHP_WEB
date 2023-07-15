06d45ff609ebd4c234fc2545f7836596c9e47e5f
Stephen <stephenirinoye@outlook.com>
2023-07-15 13:18:14 +0100

    configure environments

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    "database" => [
+        "host" => "localhost",
+        "port" => 3306,
+        "dbname" => "Kizenthia",
+        "charset" => "utf8mb4"
+    ]
+    ];
--- index.php
+++ index.php
@@ -2,4 +2,19 @@
 
 require "functions.php";
 
-require "router.php";
+// require "router.php";
+
+//connect to MySQL database
+
+require "Database.php";
+
+$config = require("config.php");
+
+$db = new Database($config["database"]);
+
+$posts = $db->query("select * from `characters`")->fetchAll();
+
+
+foreach ($posts as $post) {
+   echo "<li>" . $post["full_name"] . "</li>";
+}
