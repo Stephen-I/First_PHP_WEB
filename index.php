db48447fbb64b361e2342b30474eba7dc465d526
Stephen <stephenirinoye@outlook.com>
2023-07-18 11:46:26 +0100

    more clean up

--- index.php
+++ index.php
@@ -5,11 +5,3 @@ require "functions.php";
 require "Database.php";
 
 require "router.php";
-
-
-
-// $id = $_GET["id"] ?? null;
-
-// $query = "select * from `characters` where id = ?";
-
-// $posts = $db->query($query, [$id])->fetch();
