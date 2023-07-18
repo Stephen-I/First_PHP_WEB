fd62e8c337fbffc4e01a889d032451f593bfb56e
Stephen <stephenirinoye@outlook.com>
2023-07-18 17:17:06 +0100

    more re-organisation

--- controllers/reviews/single.php
+++ controllers/reviews/single.php
@@ -8,4 +8,8 @@ $db = new Database($config["database"]);
 
 $review = $db->query("select * from `reviews` where id = :id", ["id" => $_GET["id"]])->fetch();
 
+if(!$review){
+    abort();
+}
+
 require views("reviews/single.view.php", ["heading" => $heading, "review" => $review]);
